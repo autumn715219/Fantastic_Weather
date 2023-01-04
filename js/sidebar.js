105a0bf6ea6eff4a886f48c35a105c5751bbca6f
finleypeng <weilllyox1020@gmail.com>
2023-01-04 18:20:12 +0800

    1. 完成 600px 以下 RWD 設計 2. 找尋天氣背景圖片

---
+++ js/sidebar.js
@@ -0,0 +1,27 @@
+function sidebarSlide() {
+	const hamburger = document.querySelector(".hamburger");
+	const blockSB = document.querySelector(".blockSB");
+	const close = document.querySelector(".close");
+	const main = document.querySelector(".main");
+
+	hamburger.addEventListener("click", () => {
+		blockSB.style.animation = "slideIn 0.5s both";
+	});
+
+	close.addEventListener("click", () => {
+		blockSB.style.animation = "slideOut 0.5s both";
+	});
+
+	main.addEventListener(
+		"click",
+		() => {
+			blockSB.style.animation = "slideOut 0.5s both";
+		},
+		true
+	);
+}
+sidebarSlide();
+
+// if (window.innerWidth >= 600) {
+
+// }
